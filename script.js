07e94daa70932deb47b1bcf216c412cdbea4c0ce
idrissa260 <idrissasall260@gmail.com>
2026-05-05 19:38:15 +0200

    add animation

---
+++ script.js
@@ -0,0 +1,13 @@
+const elements = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+elements.forEach(element => observer.observe(element));
